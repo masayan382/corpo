c3261f1ecf658fc29a2721c89734e4560b02e39a
m-takahashi <masan3821031@gmail.com>
2021-08-08 14:50:47 +0900

    frontページ

---
+++ index.php
@@ -0,0 +1,52 @@
+<?php get_header(); ?>
+
+<main>
+    <article class="mainvisual">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/mv.jpg" alt="">
+        <h2>Reliable technology</h2>
+    </article>
+
+    <div class="wrapper">
+        <article class="top-greeting">
+            <section class="inner">
+                <h2>ご挨拶</h2>
+                <dl>
+                    <dt>
+                        <figure><img src="<?php echo get_template_directory_uri(); ?>/images/top_greeting_img.jpg"></figure>
+                    </dt>
+                    <dd>
+                        <p>本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。<br>
+                            本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。
+                            本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。
+                            本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。<br>
+                            本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。<br>
+                            本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。
+                        </p>
+                    </dd>
+                </dl>
+            </section>
+        </article>
+        <article class="top-business">
+            <section class="inner">
+                <h2>製品情報</h2>
+                <div>
+                    <p>本文が入ります。本文が入ります。本文が入ります。本文が入ります。本文が入ります。</p>
+                    <div class="top-business_btn"><a class="btn" href="product_line.html">詳しくはこちら</a></div>
+                </div>
+            </section>
+        </article>
+        <article class="top-news">
+            <section class="inner">
+                <h2>NEWS</h2>
+                <ul class="news-archive">
+                    <li><time>2021.00.00</time><a href="news_details.html">ニュースタイトルが入りますニュースタイトルが入りますニュースタイトルが入ります</a></li>
+                    <li><time>2021.00.00</time><a href="news_details.html">ニュースタイトルが入りますニュースタイトルが入ります</a></li>
+                    <li><time>2021.00.00</time><a href="news_details.html">ニュースタイトルが入ります</a></li>
+                </ul>
+                <div class="top-news_btn"><a class="btn btn-primary" href="news.html">NEWS一覧はこちら</a></div>
+            </section>
+        </article>
+    </div>
+</main>
+
+<?php get_footer(); ?>
