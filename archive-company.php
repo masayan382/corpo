4ab4e39d68a9b8f43d28c09380bdbd5e91436628
m-takahashi <masan3821031@gmail.com>
2021-08-08 17:36:57 +0900

    会社案内

--- archive-company.php
+++ archive-company.php
@@ -1,85 +1,68 @@
 <?php get_header(); ?>
 
-<main>
-    <article class="company_sv subvisual">
-        <h2>会社案内</h2>
-    </article>
+<?php $args = array(
+    'post_type' => 'company',
+    'p' => '42'
+);
+$posts_array = get_posts($args); ?>
+<?php foreach ($posts_array as $post) : setup_postdata($post); ?>
 
-    <section class="breadcrumb inner">
-        <ul>
-            <li><a href="index.html">TOP</a></li>
-            <li>会社案内</li>
-        </ul>
-    </section>
-
-    <div class="wrapper">
-        <article class="company_profile">
-            <section class="inner">
-                <h2>会社概要</h2>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>社名</th>
-                            <td>株式会社KURIPRESS</td>
-                        </tr>
-                        <tr>
-                            <th>所在地</th>
-                            <td>〒100-0005 東京都千代田区丸の内1丁目</td>
-                        </tr>
-                        <tr>
-                            <th>設立</th>
-                            <td>0000年　00月　00日</td>
-                        </tr>
-                        <tr>
-                            <th>資本金</th>
-                            <td>00,000,000円</td>
-                        </tr>
-                        <tr>
-                            <th>従業員数</th>
-                            <td>000人</td>
-                        </tr>
-                        <tr>
-                            <th>役員</th>
-                            <td>代表取締役　●● ●●</td>
-                        </tr>
-                        <tr>
-                            <th>事業内容</th>
-                            <td>事業内容</td>
-                        </tr>
-                        <tr>
-                            <th>関連会社</th>
-                            <td>株式会社　●●●●</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </section>
+    <main>
+        <article class="company_sv subvisual">
+            <h2>会社案内</h2>
         </article>
 
-        <article class="access_map">
-            <section class="inner">
-                <h3>アクセスマップ</h3>
-                <table>
-                    <tbody>
-                        <tr>
-                            <th>所在地</th>
-                            <td>〒000-0000</td>
-                        </tr>
-                        <tr>
-                            <th>TEL</th>
-                            <td>00-0000-0000</td>
-                        </tr>
-                        <tr>
-                            <th>FAX</th>
-                            <td>00-0000-0000</td>
-                        </tr>
-                    </tbody>
-                </table>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3240.8280303808788!2d139.76493611525882!3d35.68123618019432!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x60188bfbd89f700b%3A0x277c49ba34ed38!2z5p2x5Lqs6aeF!5e0!3m2!1sja!2sjp!4v1618227751405!5m2!1sja!2sjp" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-            </section>
-        </article>
+        <section class="breadcrumb inner">
+            <ul>
+                <li><a href="index.html">TOP</a></li>
+                <li>会社案内</li>
+            </ul>
+        </section>
 
-    </div>
+        <div class="wrapper">
+            <article class="company_profile">
+                <section class="inner">
+                    <h2><?php echo CFS()->get('company_title01'); ?></h2>
+                    <table>
+                        <tbody>
+                            <?php
+                            $fields = CFS()->get('company_tableloop01');
+                            foreach ($fields as $field) :
+                            ?>
+                                <tr>
+                                    <th><?php echo $field['company_th01']; ?></th>
+                                    <td><?php echo $field['company_td01']; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </section>
+            </article>
 
-</main>
+            <article class="access_map">
+                <section class="inner">
+                    <h3><?php echo CFS()->get('company_title02'); ?></h3>
+                    <table>
+                        <tbody>
+                            <?php
+                            $fields = CFS()->get('company_tableloop02');
+                            foreach ($fields as $field) :
+                            ?>
+                                <tr>
+                                    <th><?php echo $field['company_th02']; ?></th>
+                                    <td><?php echo $field['company_td02']; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                    <iframe src="<?php echo CFS()->get('company_map'); ?>" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                </section>
+            </article>
+        </div>
 
-<?php get_footer(); ?>
+    <?php endforeach; ?>
+    <?php wp_reset_postdata(); ?>
+
+    </main>
+
+    <?php get_footer(); ?>
