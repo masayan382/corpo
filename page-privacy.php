e128beafa12025e65dc8b9a3a1bdd6524024a13a
m-takahashi <masan3821031@gmail.com>
2021-08-22 15:52:01 +0900

    プライバシーポリシー

---
+++ page-privacy.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+<main>
+    <article class="privacy_sv subvisual">
+        <h2>プライバシーポリシー</h2>
+    </article>
+
+    <section class="breadcrumb inner">
+        <ul>
+            <li><a href="index.html">TOP</a></li>
+            <li>プライバシーポリシー</li>
+        </ul>
+    </section>
+
+    <div class="wrapper">
+        <article class="privacy_policy">
+            <section class="inner">
+                <p>KURIPRESS（以下、「当サイト」と言います。）では、お客様からお預かりする個人情報の重要性を強く認識し、個人情報の保護に関する法律、その他の関係法令を遵守すると共に、以下に定めるプライバシーポリシーに従って、個人情報を安全かつ適切に取り扱うことを宣言いたします。</p>
+                <div>
+                    <h2><?php the_title(); ?></h2>
+                    <?php the_content(); ?>
+            </section>
+        </article>
+    </div>
+</main>
+
+<?php get_footer(); ?>
