41a7c437be3a93993ef2b82d71e2fb406a37f63e
m-takahashi <masan3821031@gmail.com>
2021-08-22 15:40:26 +0900

    リクルート戻し

--- archive-recruit.php
+++ archive-recruit.php
@@ -15,31 +15,22 @@
     <div class="wrapper">
         <article class="recruit">
             <section class="inner">
-                <h2>採用情報一覧</h2>
-                <?php
-                $taxonomy = 'recruit_list';
-                $args = array(
-                    'hide_empty' => 0,
-                    'parent' => 0
-                );
-                $terms = get_terms($taxonomy, $args);
-                if ($terms) { ?>
-                    <ul>
-                        <?php foreach ($terms as $term) {
-                            $cat_image = SCF::get_term_meta($term->term_id, $taxonomy, 'recruit_list_img');
-                            $cf_sample = wp_get_attachment_image_src($cat_image, 'full');
-                            $imgUrl = esc_url($cf_sample[0]);
-                        ?>
-                            <li><a href="<?php echo get_category_link($term->term_id); ?>">
-                                    <figure><img src="<?php echo $imgUrl; ?>"></figure>
-                                    <div>
-                                        <h3><?php echo $term->name; ?></h3>
-                                    </div>
-                                </a>
-                            </li>
-                        <?php } ?>
-                    </ul>
-                <?php } ?>
+                <ul>
+                    <li><a href="recruit_list.html">
+                            <figure><img src="images/recruit_list.jpg"></figure>
+                            <div>
+                                <h3>募集要項</h3>
+                            </div>
+                        </a>
+                    </li>
+                    <li><a href="recruit_voice.html">
+                            <figure><img src="images/recruit_voice.jpg"></figure>
+                            <div>
+                                <h3>社員の声</h3>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
             </section>
         </article>
     </div>
